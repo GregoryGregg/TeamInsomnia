d2eccc9fbf0efea4b8c4e9b46a86af97bb1d2a8d
GregoryGregg_Desktop <nicholasgregg94@gmail.com>
2018-06-24 00:10:57 -0500

    Finished setup of Directional Conversion

--- TeamInsomnia.srcs/sources_1/new/Direction_Conversion.v
+++ TeamInsomnia.srcs/sources_1/new/Direction_Conversion.v
@@ -38,8 +38,40 @@ module Direction_Conversion(
    
     always @(*)
     begin
+        if (direction == 2'b11)
+        begin
+            DirA_r <= 1;
+            DirB_r <= 1;
+        end
+        if (direction == 2'b10)
+        begin
+            DirA_r <= 1;
+            DirB_r <= 0;
+        end
+        if (direction == 2'b01)
+        begin
+            DirA_r <= 0;
+            DirB_r <= 1;
+        end
         if (direction == 2'b00)
-            Dir
+        begin
+            DirA_r <= 0;
+            DirB_r <= 0;
+        end    
+    end
+    
+    Directional_Control MotorA(
+        .direction(DirA_r),
+        .brake(brake),
+        .INA(IN1),
+        .INB(IN2)
+        );
         
+    Directional_Control MotorB(
+        .direction(DirB_r),
+        .brake(brake),
+        .INA(IN3),
+        .INB(IN4)
+        );
     
 endmodule
