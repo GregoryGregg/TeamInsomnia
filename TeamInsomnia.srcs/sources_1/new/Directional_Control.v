9307a63780eb6b1b36da92f4174cf56ff2d71ace
Nicholas Gregg_Desktop <nicholasgregg94@gmail.com>
2018-06-23 23:09:46 -0500

    Added Control Files
    Added files that will control aspects of braking, direction and PWM, then there is a "Top Module" Just for motor control that can be altered to receive signals from other modules to pass to the motors

---
+++ TeamInsomnia.srcs/sources_1/new/Directional_Control.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/23/2018 11:05:39 PM
+// Design Name: 
+// Module Name: Directional_Control
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Directional_Control(
+
+    );
+endmodule
---
+++ TeamInsomnia.srcs/sources_1/new/PWM.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/23/2018 11:06:48 PM
+// Design Name: 
+// Module Name: PWM
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PWM(
+
+    );
+endmodule
