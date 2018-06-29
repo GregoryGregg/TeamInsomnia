0080c77ce67f5217f886f03442a8031603390164
GregoryGregg_Desktop <nicholasgregg94@gmail.com>
2018-06-29 16:45:31 -0500

    Changed half speed conditions

--- TeamInsomnia.srcs/sources_1/new/Motor_Control.v
+++ TeamInsomnia.srcs/sources_1/new/Motor_Control.v
@@ -38,7 +38,7 @@ module Motor_Control(
    always @(*)
    begin
    ratioA_r = ((Direction == 3'b110)||(Direction == 3'b101)) ? (32768/(2*63)):(32768/63);
-   ratioB_r = ((Direction == 3'b001)||(Direction == 3'b010)) ? (32768/(2*63)):(32768/63);
+   ratioB_r = ((Direction == 3'b011)||(Direction == 3'b010)) ? (32768/(2*63)):(32768/63);
    end
    
     PWM speedB (
