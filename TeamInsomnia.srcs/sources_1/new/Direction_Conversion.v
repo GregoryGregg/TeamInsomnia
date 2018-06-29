7075043f266c11daa27ca7360ba0b9b691c9b348
GregoryGregg_Desktop <nicholasgregg94@gmail.com>
2018-06-29 15:59:46 -0500

    Removed the if statment
    Replaced it with a case statement to make things much clearer, also to make things simpler.

--- TeamInsomnia.srcs/sources_1/new/Direction_Conversion.v
+++ TeamInsomnia.srcs/sources_1/new/Direction_Conversion.v
@@ -43,26 +43,30 @@ module Direction_Conversion(
    
     always @(*)
     begin
-      if (Direction == (3'b111||3'b110||3'b001))
-      begin
-        DirA_r <= 1;
-        DirB_r <= 0;
-      end
-      else if (Direction == (3'b000||3'b101||3'b010))
-      begin
-        DirA_r <= 0;
-        DirB_r <= 1;
-      end
-      else if (Direction == 3'b100)
-      begin
-        DirA_r <= 1;
-        DirB_r <= 1;
-      end
-      else if (Direction == 3'b001)
-      begin
-        DirA_r <= 0;
-        DirB_r <= 0;
-      end
+    
+      case(Direction)
+      //Forwards and turning
+      3'b111, 3'b110, 3'b011:
+        begin
+            DirA_r = 0;
+            DirB_r = 1;
+        end
+      3'b000, 3'b101, 3'b010:
+        begin
+            DirA_r = 1;
+            DirB_r = 0;
+        end
+      3'b100:
+        begin
+            DirA_r = 1;
+            DirB_r = 1;
+        end
+      3'b001:
+        begin
+            DirA_r = 0;
+            DirB_r = 0;
+        end
+        endcase
     end
     
     Directional_Control MotorA(
