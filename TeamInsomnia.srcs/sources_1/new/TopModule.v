22644256181c98d9f81c2a0b625aa8f88403155a
Justin Schwausch <justinschwausch980@gmail.com>
2018-06-30 11:44:05 -0500

    Added seven seg module and dependancies
    Changed xdc to allow seven seg, created seven seg instantiation in top.

--- TeamInsomnia.srcs/sources_1/new/TopModule.v
+++ TeamInsomnia.srcs/sources_1/new/TopModule.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 06/22/2018 01:13:31 PM
+// Create Date: 06/30/2018 11:39:31 AM
 // Design Name: 
 // Module Name: TopModule
 // Project Name: 
@@ -14,13 +14,27 @@
 // Dependencies: 
 // 
 // Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
+// Revision 0.02 - added seven seg and io (Justin)
+// Additional Comments: More info in seven_sev.v
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module TopModule(
+    input clk,
+    output[3:0] an,
+    output[6:0] seg
+
 
     );
+    
+    wire[15:0] msg; //wire for the message for the seven seg
+    
+     seven_seg Useven_seg( //instantiate the seven seg display
+      .clk (clk),
+      .msg (msg),
+      .an  (an),
+      .seg (seg)
+     );
+    
 endmodule
