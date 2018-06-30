ca54b936e87ff6a66172575cca372022b7ff824a
GregoryGregg_Desktop <nicholasgregg94@gmail.com>
2018-06-29 19:15:40 -0500

    Test of Coast parameter
    Succesful simulation of the coast parameter in the code

--- TeamInsomnia.sim/sim_1/behav/xsim/xsim.dir/TopModule_behav/webtalk/xsim_webtalk.tcl
+++ TeamInsomnia.sim/sim_1/behav/xsim/xsim.dir/TopModule_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/nicho/Documents/Vivado_Projects/TeamInsomnia/TeamInsomnia.sim/sim_1/behav/xsim/xsim.dir/TopModule_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri Jun 29 16:55:21 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Fri Jun 29 19:15:12 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2188600" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "4898c971-4366-4593-ba0e-33fb79538f98" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "08756667e20d4bfebca46b2dff0fdc26" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "36" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "40" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-7700 CPU @ 3.60GHz" -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "17.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "9001 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "5001 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.83_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "13732_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3000622816 -regid "" -xml C:/Users/nicho/Documents/Vivado_Projects/TeamInsomnia/TeamInsomnia.sim/sim_1/behav/xsim/xsim.dir/TopModule_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/nicho/Documents/Vivado_Projects/TeamInsomnia/TeamInsomnia.sim/sim_1/behav/xsim/xsim.dir/TopModule_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/nicho/Documents/Vivado_Projects/TeamInsomnia/TeamInsomnia.sim/sim_1/behav/xsim/xsim.dir/TopModule_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.47_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "13768_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 298240 -regid "" -xml C:/Users/nicho/Documents/Vivado_Projects/TeamInsomnia/TeamInsomnia.sim/sim_1/behav/xsim/xsim.dir/TopModule_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/nicho/Documents/Vivado_Projects/TeamInsomnia/TeamInsomnia.sim/sim_1/behav/xsim/xsim.dir/TopModule_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/nicho/Documents/Vivado_Projects/TeamInsomnia/TeamInsomnia.sim/sim_1/behav/xsim/xsim.dir/TopModule_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
