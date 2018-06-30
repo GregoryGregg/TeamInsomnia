c8ee7865f7497b052d37ea8f9ec9616000704214
Justin Schwausch <justinschwausch980@gmail.com>
2018-06-30 13:08:14 -0500

    Added ultrasonic and IO

--- TeamInsomnia.srcs/sources_1/new/TopModule.v
+++ TeamInsomnia.srcs/sources_1/new/TopModule.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 06/30/2018 11:39:31 AM
+// Create Date: 06/30/2018 01:07:31 PM
 // Design Name: 
 // Module Name: TopModule
 // Project Name: 
@@ -14,21 +14,24 @@
 // Dependencies: 
 // 
 // Revision:
-// Revision 0.02 - added seven seg and io (Justin)
-// Additional Comments: More info in seven_sev.v
+// Revision 0.03 - added ultrasonic and io (Justin)
+// Additional Comments: More info in ultrasonic_proximity.v
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module TopModule(
     input clk,
-    output[3:0] an,
-    output[6:0] seg
-
+    input JA0, //echo from ultrasonic
+    output[3:0] an, //anode for seven seg
+    output[6:0] seg, //segment for seven seg
+    output JA1 //trigger for ultrasonic
+    
 
     );
     
     wire[15:0] msg; //wire for the message for the seven seg
+    wire[15:0] dist; //distance from proximity sensor
     
      seven_seg Useven_seg( //instantiate the seven seg display
       .clk (clk),
@@ -36,5 +39,14 @@ module TopModule(
       .an  (an),
       .seg (seg)
      );
+     
+     ultrasonic_proximity Uultrasonic_proximity( //instantiate the ultrasonic sensor
+      .clk  (clk),
+      .JA0  (JA0),
+      .JA1  (JA1),
+      .dist (dist)
+      );
+      
+     
     
 endmodule
---
+++ TeamInsomnia.srcs/sources_1/new/ultrasonic_proximity.v
@@ -0,0 +1,79 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Name: Justin Schwausch
+// Class: ECE 3331
+// Project: Ultrasonic Sensor
+// Target: Basys 3 Board
+// Last Modified: 6/30/2018
+//
+// This program takes makes use of the HC-SR04 to detect proximity.
+//
+// The relative distance is displayed on the four seven segment displays on the Basys board
+//  and on the 16 LEDs on the board.
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module ultrasonic_proximity(
+
+input clk, //main clock
+input JA0, // echo pin
+output JA1, //trigger pin
+output dist //distance from object
+    );
+    
+    reg counten; //count enable reg
+    reg outen; //out enable reg
+    reg[21:0] count; //count reg
+    reg[15:0] countf; //stores the 16 most significant bits of count
+    reg[9:0] outcnt; //counts out trigger pulse
+    reg[25:0] delcnt = 26'b0; //counts delay between measurements
+    wire outtogg;
+
+    assign JA1 = outen; //assign trigger output
+    assign dist = countf; //assign output distance
+    assign outtogg = (delcnt == 26'b0) ?1'b1:1'b0; //begin trigger pulse
+    
+    
+    always @(posedge clk)
+    begin
+    
+    if(outcnt != 10'b0000000000) //if already counting
+    begin
+    outcnt = outcnt + 1'b1; //continue counting
+    end
+    
+    else if(outtogg == 1'b1) //if supposed to be counting
+    begin
+    outcnt = 1'b1; //start counting
+    outen = 1'b1; //trigger posedge
+    end
+    
+    if(outcnt == 10'b1111101000) //if trigger on for 10 microseconds
+    begin
+    outcnt = 10'b0000000000; //reset count
+    outen = 1'b0; //trigger negedge
+    end
+    
+    delcnt <= delcnt + 1'b1; //up delay count
+    
+    end
+    
+    
+    always @(posedge clk)
+    begin
+    if (JA0) //if echo detected
+    begin
+        count <= count + 1; //count echo pulse width
+    end
+    
+    else if(count != 22'b0) //if count isn't 0
+    begin
+    
+        countf <= count[21:6]; //load 16 most sig bits into countf
+        count <= 22'b0; //reset count
+    end
+
+  end
+        
+endmodule
