48332c882f65db1e284429af4c8407dd9212c242
GregoryGregg_Desktop <nicholasgregg94@gmail.com>
2018-06-30 22:37:15 -0500

    Added Motor control to top module
    added the io for motor control to the top module, instantiated it as Surface, no other code should be needed to control the motor other than surface

--- TeamInsomnia.srcs/sources_1/new/TopModule.v
+++ TeamInsomnia.srcs/sources_1/new/TopModule.v
@@ -22,31 +22,54 @@
 
 module TopModule(
     input clk,
+    input [5:0]sw, // Speed for the motor control module set by the switches, to be removed
+    input brake, // to be removed in favor of a register in the code
+    input coast, // to be removed as with brake
     input JA0, //echo from ultrasonic
     output[3:0] an, //anode for seven seg
     output[6:0] seg, //segment for seven seg
-    output JA1 //trigger for ultrasonic
-    
-
-    );
+    output JA1, //trigger for ultrasonic
+    output IN1, // All that follow are motor outputs and shouldn't be moved
+    output IN2, 
+    output IN3,
+    output IN4,
+    output ENA,
+    output ENB
+);
     
     wire[15:0] msg; //wire for the message for the seven seg
     wire[15:0] dist; //distance from proximity sensor
     
+    reg[2:0] Direction; // I suggest using reg rather than wire
+    
      seven_seg Useven_seg( //instantiate the seven seg display
-      .clk (clk),
-      .msg (msg),
-      .an  (an),
-      .seg (seg)
+        .clk (clk),
+        .msg (msg),
+        .an  (an),
+        .seg (seg)
      );
      
      ultrasonic_proximity Uultrasonic_proximity( //instantiate the ultrasonic sensor
-      .clk  (clk),
-      .echo  (JA0),
-      .trigger  (JA1),
-      .dist (dist)
+        .clk  (clk),
+        .echo  (JA0),
+        .trigger  (JA1),
+        .dist (dist)
       );
       
+      // Motor control instantiaiton, Keep this at the bottom
+      Motor_Control Surface (
+        .Direction(Direction),
+        .clk(clk),
+        .sw(sw),    // to be removed 
+        .brake(brake),
+        .coast(coast),
+        .IN1(IN1),
+        .IN2(IN2),
+        .IN3(IN3),
+        .IN4(IN4),
+        .ENA(ENA),
+        .ENB(ENB)
+     );
      
     
 endmodule
